0d2ff34f9420b038e493832b450ed9eaca28a9e9
Guilherme Figueiredo Terenciani <guilhermeterenciani@gmail.com>
2024-03-27 08:45:23 -0400

    Update Main.tsx

--- src/componentes/main/Main.tsx
+++ src/componentes/main/Main.tsx
@@ -1,98 +1,171 @@
-import './Main.css'
-import Estudante from '../estudante/Estudante'
-function Main(){
-    return(
-        <main>
-            <Estudante 
-                nome="Guilherme Figueiredo Terenciani"
-                imagem="https://github.com/guilhermeterenciani.png"
-                descricao="Professor do IFMS nas matérias de Desenvolvimento - Desenvolvimento Web"
-                githubLink='https://github.com/guilhermeterenciani'
-            />
-<Estudante 
+import "./Main.css";
+import Estudante from "../estudante/Estudante";
+function Main() {
+  return (
+    <main>
+      <Estudante
+        nome="Marcela Santos de Oliveira"
+        imagem="https://github.com/marcelaoliveira2.png"
+        descricao="Estudante do IFMS"
+        githubLink="https://github.com/marcelaoliveira2"
+      />
+      <Estudante
+        nome="Guilherme Figueiredo Terenciani"
+        imagem="https://github.com/guilhermeterenciani.png"
+        descricao="Professor do IFMS nas matérias de Desenvolvimento - Desenvolvimento Web"
+        githubLink="https://github.com/guilhermeterenciani"
+      />
+      <Estudante
+        nome="Vitória"
+        imagem="https://github.com/07174293182.png"
+        descricao="Estudante do IFMS"
+        githubLink="https://github.com/07174293182"
+      />
+      <Estudante
+        nome="Guilherme Pinheiro"
+        imagem="https://github.com/GuiPiinheiro.png"
+        descricao="Estudante do IFMS que não aguenta mais essa vida!"
+        githubLink="https://github.com/GuiPiinheiro"
+      />
 
-            nome="Vitória"
-            imagem="https://github.com/07174293182.png"
-            descricao= "Estudante do IFMS"
-            githubLink= "https://github.com/07174293182"
-            />
-<Estudante 
-            nome='Guilherme Pinheiro'
-            imagem='https://github.com/GuiPiinheiro.png'
-            descricao='Estudante do IFMS que não aguenta mais essa vida!'
-            githubLink='https://github.com/GuiPiinheiro'/>
+      <Estudante
+        nome="Lucas"
+        imagem="https://github.com/lucasfelipeoliveira2022.png"
+        descricao="Estudante do IFMS em Técnico de Informática para Internet"
+        githubLink="http://github.com/lucasfelipeoliveira2022"
+      />
 
-<Estudante
-          nome='Lucas'
-          imagem='https://github.com/lucasfelipeoliveira2022.png'
-          descricao='Estudante do IFMS em Técnico de Informática para Internet'
-          githubLink='http://github.com/lucasfelipeoliveira2022'
-          />
+      <Estudante
+        nome="Maria Clara"
+        imagem="https://github.com/mariFeldk.png"
+        descricao="estudante do IFMS informatica para internet"
+        githubLink="https://github.com/mariFeldk"
+      />
 
- <Estudante 
+      <Estudante
+        nome="MarcUs Gamer"
+        imagem="https://github.com/linss22.png"
+        descricao="Gamer do IFMS hahahahahahah"
+        githubLink="https://github.com/linss22"
+      />
 
-            nome="Maria Clara" 
-            imagem = "https://github.com/mariFeldk.png"
-            descricao = "estudante do IFMS informatica para internet"
-            githubLink="https://github.com/mariFeldk"/>
-            
-<Estudante 
-            nome="MarcUs Gamer"
-            imagem='https://github.com/linss22.png'
-            descricao='Gamer do IFMS hahahahahahah'
-            githubLink='https://github.com/linss22'
-            />    
-          
-<Estudante 
-                nome="Gustavo A."
-                imagem="https://github.com/torneira.png"
-                descricao="GG"
-                githubLink="https://github.com/torneira"
-                />
+      <Estudante
+        nome="Gustavo A."
+        imagem="https://github.com/torneira.png"
+        descricao="GG"
+        githubLink="https://github.com/torneira"
+      />
 
+      <Estudante
+        nome="Victor Hugo Da Costa Bueno"
+        imagem="https://github.com/victor17y.png"
+        descricao="estudante do ifms nas materias "
+        githubLink='href="https://github.com/victor17y'
+      />
 
-<Estudante 
-                nome="Cãosidente"
-                imagem='http://github.com/sh0uwzz.png'
-                descricao='Presidente do Brasil'
-                githubLink='http://github.com/sh0uwzz'
-            />
-<Estudante 
-                nome="João Victor da Silva Caetano"
-                imagem="https://github.com/joaovscaetano.png"
-                descricao="Estudante do IFMS nas matérias de Desenvolvimento - Desenvolvimento Web"
-                githubLink="https://github.com/joaovscaetano"
-            />
+      <Estudante
+        nome="Luis Guilherme"
+        imagem="http://github.com/sh0uwzz.png"
+        descricao="Presidente do Brasil"
+        githubLink="http://github.com/sh0uwzz"
+      />
+      <Estudante
+        nome="João Victor da Silva Caetano"
+        imagem="https://github.com/joaovscaetano.png"
+        descricao="Estudante do IFMS nas matérias de Desenvolvimento - Desenvolvimento Web"
+        githubLink="https://github.com/joaovscaetano"
+      />
 
+      <Estudante
+        nome="Vinicius Eduardo"
+        imagem="https://github.com/vinixxsdj.png"
+        descricao="Estudante do IFMS"
+        githubLink="https://github.com/vinixxsdj"
+      />
 
-  <Estudante 
-               nome="Vinicius Eduardo"
-               imagem="https://github.com/vinixxsdj.png"
-               descricao="Estudante do IFMS"
-               githubLink="https://github.com/vinixxsdj"/>
-              
+      <Estudante
+        nome="Felipe Santos"
+        imagem="https://github.com/Felipe10222b.png"
+        descricao="vampetinhacria"
+        githubLink="https://github.com/Felipe10222b"
+      />
+      <Estudante
+        nome="Rikelme de Freitas"
+        imagem="https://github.com/rikelme1810.png"
+        descricao="Estudante vagabundo"
+        githubLink="https://github.com/rikelme1810"
+      />
 
- <Estudante 
-                nome="Felipe Santos"
-                imagem="https://github.com/Felipe10222b.png"
-                descricao="vampetinhacria"
-                githubLink='https://github.com/Felipe10222b'
-            />
-<Estudante
-                nome="Rikelme de Freitas"
-                imagem='https://github.com/rikelme1810.png'
-                descricao='Estudante vagabundo'
-                githubLink='https://github.com/rikelme1810'
-                />
-            
+      <Estudante
+        nome="Gustavo Oliveira Silva"
+        imagem="https://github.com/Gustavoif12.png"
+        descricao="Aluno do IFMS nas matérias de Desenvolvimento / Desenvolvimento Web"
+        githubLink="https://github.com/Gustavoif12"
+      />
+      <Estudante
+        nome="Suellyn"
+        descricao="Estudante IFMS - curso integrado
+                Técnico de Informática para a Internet"
+        imagem="https://github.com/suxxc.png"
+        githubLink="https://github.com/suxxc"
+      />
+      <Estudante
+        nome="Ryan Cristian"
+        imagem="https://github.com/trycsr.png"
+        descricao="Estudante do IFMS cursando a area de TI"
+        githubLink="https://github.com/trycsr"
+      />
 
-<Estudante
-                nome="Gustavo Oliveira Silva"
-                imagem="https://github.com/Gustavoif12.png"
-                descricao="Aluno do IFMS nas matérias de Desenvolvimento / Desenvolvimento Web"
-                githubLink="https://github.com/Gustavoif12"
-                />
-        </main>
-    )
+      <Estudante
+        nome="Ana Clara Eleutério"
+        imagem="https://github.com/annawlyzz.png"
+        descricao="Aluna do IFMS, no 5° semestre do Curso Técnico de Informática para Internet"
+        githubLink="https://github.com/annawlyzz"
+      />
+      <Estudante
+        nome="Gabriela Silva Palma"
+        imagem="https://github.com/gabisp28.png"
+        descricao="Estudante do Instituto Federal do Mato Grosso do Sul, estou no 5º Semestre, cursando Técnico em Informática para Internet"
+        githubLink="https://github.com/gabisp28"
+      />
+      <Estudante
+        nome="Vitória Dangui Siqueira"
+        imagem="https:////github.com/vitoriasiqueira.png"
+        descricao="Estudante de Informática para Internet no 5° Semestre"
+        githubLink="https://github.com/vitoriasiqueira"
+      />
+      <Estudante
+        nome="Guilherme Finoto"
+        imagem="https://github.com/FinotoZx.png"
+        descricao="Estudante do IFMS"
+        githubLink="https://github.com/FinotoZx"
+      />
+      <Estudante
+        nome="Felipe Brito Ferreira"
+        imagem="https://github.com/FelipeBrito1234.png"
+        descricao="Estudante do IFMS nas matérias de Desenvolvimento - Desenvolvimento Web"
+        githubLink="https://github.com/FelipeBrito1234"
+      />
+      <Estudante
+        nome="Maria Rodrigues"
+        imagem="https://github.com/07174293182.png"
+        descricao="Estudante do IFMS"
+        githubLink="https://github.com/07174293182"
+      />
+      <Estudante
+        nome="Rebeca Navier Coutinho"
+        imagem="https://github.com/NavierK.png"
+        descricao="Remeleca lore estudante do IFMS"
+        githubLink="https://github.com/NavierK"
+      />
+      <Estudante
+        nome="Isabely Antunes da Silva"
+        imagem="https://github.com/IsabelyAntunes.png"
+        descricao=" Estudante do IFMS nas matérias de 
+            Desenvolvimento - Desenvolvimento Web"
+        githubLink="https://github.com/IsabelyAntunes"
+      />
+    </main>
+  );
 }
-export default Main
+export default Main;
